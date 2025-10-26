97588803b2cb75b319d2680bb89da14da4bd167b
indrasikhda <indrastoikhdanuhaad@mail.ugm.ac.id>
2025-10-26 20:51:44 +0700

    menambahkan array

--- index.php
+++ index.php
@@ -48,6 +48,46 @@ $nama[2] = "Jono";
 echo $nama[1] . " dan " . $nama[2] . " adalah tetangga Pak " . $nama[0];
 echo "<br><br>";
 
+// Array Asosiatif
+$umur = array("Joko"=>33, "Parto"=>35, "Jono"=>29);
+echo "Umur Joko adalah " . $umur['Joko'] . " tahun<br>";
+echo "Umur Parto adalah " . $umur['Parto'] . " tahun<br>";
+echo "Umur Jono adalah " . $umur['Jono'] . " tahun<br>";
+echo "<br>";
+
+// Cara lain membuat array asosiatif
+$umur['Joko'] = "33";
+$umur['Parto'] = "35";
+$umur['Jono'] = "29";
+echo "Umur Joko (cara kedua) adalah " . $umur['Joko'] . " tahun<br>";
+echo "<br><br>";
+
+// Array Multidimensi
+$keluarga = array
+(
+    "Joko"=>array
+    (
+        "Jojon",
+        "Joni",
+        "Joana"
+    ),
+    "Parto"=>array
+    (
+        "Parmi"
+    ),
+    "Warto"=>array
+    (
+        "Warman",
+        "Warno",
+        "Warmin"
+    )
+);
+
+echo $keluarga["Joko"][2] ." adalah anggota keluarga Joko";
+echo "<br>";
+echo "Anak pertama Parto adalah " . $keluarga["Parto"][0];
+echo "<br><br>";
+
 // Menggunakan if..else
 $d = date("D");
 if ($d == "Sat") {
@@ -149,5 +189,7 @@ Umur: <input type="text" name="umur" />
 <input type="submit" value="Kirim"/>
 </form>
 HTML;
+
+echo "<br><br>";
 
 ?>
